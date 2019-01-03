bce8621ef070a2f16c83857f9d90c9426f1bca0b
Kubra Guclu <kubraguclu.1994@gmail.com>
2019-01-03 18:40:25 +0200

    SeoWidget ekle

---
+++ widget/SeoWidget.php
@@ -0,0 +1,42 @@
+<?php
+namespace kouosl\portal-seo\widgets;
+
+class SeoWidget extends CWidget
+{
+
+    protected $_title;
+    protected $_description;
+    protected $_keywords;
+    
+    public function init()
+    {
+        $behavior = $this->controller->asa('seo');
+     
+        if ($behavior !== null && $behavior->title !== null)
+            $this->_title->title = $behavior->title;
+
+        if ($behavior !== null && $behavior->metaDescription !== null)
+            $this->_description = $behavior->description
+
+        if ($behavior !== null && $behavior->metaKeywords !== null)
+            $this->_keywords = $behavior->keywords;
+    }
+
+    public function run()
+    {
+        $this-optimizeSeo();
+    }
+
+    /**
+     * Seo taglerini olusturur
+     */
+    protected function optimizeSeo()
+    {
+        if ($this->_title !== null)
+            echo CHtml::tag('title', array(), is_array($this->_title) ? implode($this->titleSeparator, $this->_title) : $this->_title);
+        if ($this->_description !== null)
+            echo CHtml::metaTag($this->_description, 'description');
+        if ($this->_keywords !== null)
+            echo CHtml::metaTag($this->_keywords, 'keywords');
+    }
+}
