0b743b6be69c78ad257f7bf0d589de38626c0f9e
Kubra Guclu <kubraguclu.1994@gmail.com>
2019-01-03 18:52:36 +0200

    Seo icin behaviors ekle

---
+++ behaviors/KouSeoBehavior.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Kubra Guclu <kubraguclu.1994@gmail.com>
+ */
+
+class KouSeoBehavior extends CBehavior
+{
+    /**
+     * @property Sayfa basligi
+     */
+    public $title;
+    /**
+     * @property Sayfa icerigi
+     */
+    public $description;
+    /**
+     * @property Sayfanin anahtar kelimeleri
+     */
+    public $keywords;
+}
